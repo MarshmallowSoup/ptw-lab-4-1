24ae586ca0d512c47bfb2c075bd025ff812bd807
MarshmallowSoup <lysetskyyv@gmail.com>
2022-04-06 17:14:51 +0300

    initial commit

---
+++ Lab4_1/Lab4_1.cpp
@@ -0,0 +1,32 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <stdio.h>
+using namespace std;
+int count(string s) {
+	int pos = 0;
+	int k = 0;
+	while (s.find_first_of("BASIC", pos) != -1) {
+		pos = s.find_first_of("BASIC", pos) + 1;
+		k++;
+	}
+	return k;
+}
+
+string readFile(string path) {
+	string s;
+	getline(ifstream(path), s, '\0');
+	return s;
+}
+
+int main()
+{
+	string filename = "1.txt";
+	string fileContent = readFile(filename);
+	int result = count(fileContent);
+	cout << "Number of 'BASIC' in file: " << result << endl;
+	return 0;
+
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,19 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab4_1/Lab4_1.cpp"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			string s = "ASS";
+			int k = count(s);
+			Assert::AreEqual(3, k);
+		}
+	};
+}
